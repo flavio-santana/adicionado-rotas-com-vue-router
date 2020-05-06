640672578bf5d85bbe3f3d600b23d096437563c2
Flavio Santana <f.o.santana@gmail.com>
2020-05-05 21:03:01 -0300

    Atualizando router.js

--- src/router.js
+++ src/router.js
@@ -4,51 +4,45 @@ import VueRouter from 'vue-router'
 // Importanto components
 import Home            from './views/Home.vue'
 import Contatos        from './views/contatos/Contatos.vue'
+import Error404        from './views/Error404.vue'
 import ContatoDetalhes from './components/contatos/ContatoDetalhes.vue'
 import ContatosHome    from './components/contatos/ContatosHome.vue'
 import ContatoEditar   from './components/contatos/ContatoEditar.vue'
+import Error404Contatos  from './components/contatos/Error404Contatos.vue'
 
 // Importanto components usuários
 import Usuarios        from './components/usuarios/Usuarios.vue'
 import UsuarioDetalhes from './components/usuarios/UsuarioDetalhes.vue'
 
+//
 Vue.use(VueRouter)
 
+//
 export default new VueRouter({
     mode:'history',
     //mode:'hash', // com a # na url
     linkExactActiveClass:'active',
     routes: [
-      { 
-        path : '/', 
-        component: Home,
-        alias : '/home' 
-      }, // meusite.com
+      { path : '/', component: Home, alias : '/home' }, // meusite.com
       { 
         path : '/contatos', 
         component: Contatos,
         alias: '/meus-contatos',
         children:[
-          { 
-            path : ':id', 
-            component: ContatoDetalhes, 
-            name: 'contato' 
-          }, // meusite.com/contatos/id 
-          { 
-            path : ':id/editar', 
-            components: {
+          { path : ':id', component: ContatoDetalhes, name: 'contato' }, // meusite.com/contatos/id 
+          { path : ':id/editar', 
+          components: {
               default: ContatoEditar,
               'contato-detalhes': ContatoDetalhes
             }
           }, // meusite.com/contatos/id/editar
-          { 
-            path : '', 
-            component: ContatosHome 
-          }, 
+          { path : '', component: ContatosHome },
+          { path : '/contatos*', component: Error404Contatos }  //especifico para contatos
         ] 
       }, // meusite.com/contatos 
        
       { path : '/usuarios', component: Usuarios }, // meusite.com/usuarios 
       { path : '/usuarios/:id', component: UsuarioDetalhes }, // meusite.com/usuarios/id 
+      { path : '*', component: Error404 }
     ]
   })
