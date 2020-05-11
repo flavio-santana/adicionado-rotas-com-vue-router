24fe381e292f2247badbe1356b8e38b054f882a8
Flavio Santana <f.o.santana@gmail.com>
2020-05-11 19:09:19 -0300

    Guarda de rota before update implementada

--- src/components/contatos/ContatoDetalhes.vue
+++ src/components/contatos/ContatoDetalhes.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h3 class="font-weight-light">Detalhes Sobre o Contato com id: ( {{ id }} )</h3>
+        <p>{{ parametros }}</p>
         <router-link
         :to="`/contatos/${id}/editar`"
         class="btn btn-primary">
@@ -18,8 +19,23 @@ export default {
             required: true
         }
     },
-    created(){
-        console.log('Parâmetros da rota: ', this.$props)
+    data(){
+        return {
+            parametros : this.$route.params
+        }
+    },
+    /**
+     * Essa guarda somente será executada quando a rota que renderiza 
+     * esse component for alterada(parâmetro)
+     */
+    beforeRouteUpdate(to, from, next){
+
+        console.log('beforeRouteUpdate')
+
+        this.parametros = to.params
+
+        next()
+
     }
 }
 </script>
